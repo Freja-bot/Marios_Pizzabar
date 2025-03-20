2991642652965f2b5198a2cdd928228352428c69
Frede-72 <frjo0005@stud.kea.dk>
2025-03-20 11:42:30 +0100

    Added: DishDescription-class

---
+++ src/DishLogic/DishDescription.java
@@ -0,0 +1,40 @@
+package DishLogic;
+
+import java.util.Collections;
+import java.util.Comparator;
+
+public class DishDescription {
+
+    private int dishID;
+    private String name;
+    private double price;
+
+    public DishDescription(int dishID, String name, double price) {
+
+        this.dishID = dishID;
+        this.name = name;
+        this.price = price;
+
+    }
+
+    public DishDescription(String lineFromFile) {
+        String[] createDish = lineFromFile.split(":");
+        if (createDish.length == 3) {
+            this.dishID = Integer.parseInt(createDish[0]);
+            this.name = createDish[1];
+            this.price = Double.parseDouble(createDish[2]);
+        }
+    }
+
+    public String toString() {
+        return dishID + ". " + name + ": " + price + ",-";
+    }
+
+    public int getDishID() {
+        return dishID;
+    }
+
+    public String addToFile() {
+        return dishID + ":" + name + ":" + price;
+    }
+}
