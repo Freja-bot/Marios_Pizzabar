3888ba3f375d3b1e21ec738cb1e76051578aeba6
Frede-72 <frjo0005@stud.kea.dk>
2025-03-20 16:17:11 +0100

    Userinput update;

--- src/Main.java
+++ src/Main.java
@@ -8,70 +8,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("1 - new file, 2 - existing file");
-        int fileChoice = sc.nextInt();
-        sc.nextLine();
-        System.out.println("enter filename");
-        String fileName = sc.nextLine();
-        if (fileChoice == 1) {
-            Ledger.createFile(fileName);
-        }
-        if (fileChoice == 2) {
-            Menu.loadMenuFromFile(fileName);
-        }
-        /*DishDescription dd = new DishDescription(1, "Pizza", 599);
-        DishDescription vin = new DishDescription(2, "Vin", 99);
-        DishDescription de = new DishDescription(3,"vand",49.99);*/
-
-        while (true) {
-            System.out.println("1 - show menu, 2 - remove dish, 3 add dish, 4 sort menu");
-            int choice = sc.nextInt();
-            sc.nextLine();
-            if (choice == 1) {
-                Menu.showMenu();
-            }
-            if (choice == 2) {
-                System.out.println("enter id to remove");
-                int remove = sc.nextInt();
-                sc.nextLine();
-                Menu.removeDish(remove, fileName);
-            }
-            if (choice == 3) {
-                System.out.println("enter id");
-                int id = sc.nextInt();
-                sc.nextLine();
-                System.out.println("enter name");
-                String name = sc.nextLine();
-                System.out.println("enter price");
-                double price = sc.nextDouble();
-                sc.nextLine();
-                Menu.addNewDishToMenu(new DishDescription(id, name, price), fileName);
-            }
-            if(choice==4){
-                Menu.sort(fileName);
-            }
-        }
+        startProgram(sc);
     }
 
-    public static void startProgram(Scanner scanner){
+
+    public static void startProgram(Scanner scanner) {
         int choiceAmount = 2;
         int userChoice;
         boolean isrunning = true;
         Controller controller = new Controller(scanner);
 
         while (isrunning) {
-
+            System.out.println("print activeOrders next to menu\n1. Tilføj odre\n2. Luk program\nVælg en mulighed ve at skrive et tal");
             userChoice = controller.getUserInput(choiceAmount);
             System.out.println(userChoice);
 
             switch (userChoice) {
                 case 1: {
                     System.out.println("Indtast afhentingstidspunktet\nTime:");
-                    int hour = controller.getUserInput(23,0);
+                    int hour = controller.getUserInput(23, 0);
+                    System.out.println(hour);
                     System.out.println("Minut:");
-                    int minute = controller.getUserInput(59,0);
-                    controller.newOrder(hour, minute);
-
+                    int minute = controller.getUserInput(59);
+                    //controller.newOrder(hour, minute);
+                    System.out.println(hour);
+                    System.out.println(minute);
                     break;
                 }
                 case 2: {
@@ -88,5 +49,5 @@ public class Main {
 
         scanner.close();
     }
-
 }
+
